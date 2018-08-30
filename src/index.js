0ed1a47fe6ee686c752b061b7afd8b9ef36b055d
Joseph Boyle <boyle.p.joseph@gmail.com>
2018-08-30 12:42:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './style.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {EatonColors, EatonThemes} from '@pxblue/themes/react/src';
+import {EatonThemes} from '@pxblue/themes/react/src';
 
 render(
   <MuiThemeProvider theme={createMuiTheme(EatonThemes.blue)}>
