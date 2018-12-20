3837c399e66267dc72e504745267e22feb22950b
shivani Pitta <shivanipitta@shivanis-MacBook-Pro.local>
2018-12-20 11:20:34 -0700

    resolved issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { VERSION } from '@angular/material';
+import { moveItemInArray, CdkDragDrop} from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'material-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
+
 })
-export class AppComponent {
-  title = 'sortable-list';
+export class AppComponent implements OnInit {
+
+  version = VERSION;
+
+  data = [{
+    'name': 'George Washington',
+    'details': '1789'
+  },
+  {
+    'name': 'John Adams',
+    'details': '1796'
+  },
+  { 'name': 'Thomas Jefferson', 'details': '1800' },
+  { 'name': 'James Madison', 'details': '1808' },
+  { 'name': 'James Monroe', 'details': '1812' }
+  ];
+  item: any = "";
+  draggable: boolean = false;
+
+  displayedColumns: string[] = ['name', 'details'];
+  dataSource: any;
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.data, event.previousIndex, event.currentIndex);
+  }
+  edit() {
+    this.draggable = !this.draggable;
+   
+  }
+
+  ngOnInit(): void {
+
+  }
+  onOpenMenu(menu: any): void {
+    console.log(menu);
+  }
+  onSelected(item: any) {
+   
+    this.item = item;
+  }
+  isSelected(item: any) {
+    return this.item === item;
+  }
+ 
 }
